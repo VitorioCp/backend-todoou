f5571a8ac3fb9670314093ecd3c00c0019118370
Vitório Corrêa <97925377+VitorioCp@users.noreply.github.com>
2025-08-13 11:22:02 -0300

    fix: Bd

--- src/models/connection.js
+++ src/models/connection.js
@@ -1,31 +1,17 @@
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
-    port: process.env.DB_PORT,
-    // Configurações de SSL (comentadas)
-    /*
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-    },
-    */
-    logging: false, 
-  }
-);
+
+const sequelize = new Sequelize({
+  dialect: "sqlite",
+  storage: process.env.DB_STORAGE || "./database.sqlite", // caminho do arquivo
+  logging: false, 
+});
 
 sequelize
   .authenticate()
   .then(() => {
-    console.log("Conexão estabelecida com sucesso.");
+    console.log("Conexão com SQLite estabelecida com sucesso.");
   })
   .catch((err) => {
     console.error("Não foi possível conectar ao banco de dados:", err);
@@ -33,7 +19,7 @@ sequelize
 
 const syncDatabase = async () => {
   try {
-    await sequelize.sync({ force: true }); // Use `force: true` para recriar as tabelas
+    await sequelize.sync({ force: true }); // recria tabelas
     console.log("Tabelas sincronizadas com sucesso.");
   } catch (error) {
     console.error("Erro ao sincronizar o banco de dados:", error);
